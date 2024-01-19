ea8066b3d696aea86b359a7062de636d669bf526
이명재 <arkk20000@gmail.com>
2024-01-19 19:42:01 +0900

    feat: 클라이언트 url 담은 .env 구성

--- src/app.ts
+++ src/app.ts
@@ -16,11 +16,10 @@ import {
 } from "./handlers";
 
 const app: Express = express();
+
 const server = createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173", "http://localhost:5174"],
-  },
+  cors: { origin: ["http://localhost:4173", process.env.CLIENT_URL!] },
 });
 
 const port = 8080;
@@ -41,5 +40,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: Server is running at https://121.174.55.140:${port}`);
 });
